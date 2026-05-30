8ae5cfb593359d87e4a1857065fdda2546827cb2
Sora Harada <92529068+Saku052@users.noreply.github.com>
2026-05-30 12:14:09 +0900

    update test code

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,7 @@ impl KvStore {
     // dataの変更権がほしい
     // keyだけでいいね。keyは参照だけでいいし、変更もしない。
     pub fn delete(&mut self, key: &[u8]) {
-        self.data.remove(key);
+        self.data.remove(key);  //TODO: 削除できたことをreturnするべき
     }
 
     // get
@@ -37,4 +37,13 @@ impl KvStore {
     pub fn get(&self, key: &[u8]) -> Option<&[u8]> {
         self.data.get(key).map(|v| v.as_slice())
     }
+}
+
+// TODO: modで囲んだ方がいい
+#[test]
+fn set_and_get () {
+    let mut kvstore = KvStore::new();
+
+    kvstore.insert(b"key".to_vec(), b"value".to_vec());
+    assert_eq!(kvstore.get(b"key"), Some(b"value".as_slice()));
 }
