4dbf6f091aca05bbcc70be7d2e03769765b1760d
Sora Harada <92529068+Saku052@users.noreply.github.com>
2026-06-02 20:24:36 +0900

    unwrap for utf8

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use crateq::KvStore;
+use std::str::from_utf8;
+
 fn main() {
-    println!("Hello, world!");
+    let mut store = KvStore::new();
+
+    //2件ほどデータを入れてみる
+    let key1: Vec<u8> = b"myKey1".to_vec();
+    let key2: Vec<u8> = b"myKey2".to_vec();
+    let value1: Vec<u8> = b"value1".to_vec();
+    let value2: Vec<u8> = b"value2".to_vec();
+
+    store.insert(key1, value1);
+    store.insert(key2, value2);
+
+    let value_result1: &[u8] = store.get(b"myKey1").expect("no value found");
+
+    print!("{}", from_utf8(value_result1).unwrap());
 }
